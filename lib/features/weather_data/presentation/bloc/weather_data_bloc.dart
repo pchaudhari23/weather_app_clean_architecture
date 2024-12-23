4ee6657849e71de276275813116975ccccc70edb
Pritam <pritam2310@gmail.com>
2024-12-24 00:28:38 +0530

    Added end to end flow

--- lib/features/weather_data/presentation/bloc/weather_data_bloc.dart
+++ lib/features/weather_data/presentation/bloc/weather_data_bloc.dart
@@ -1,32 +1,3 @@
-// import 'package:bloc/bloc.dart';
-// // import 'package:equatable/equatable.dart';
-
-// import '../../domain/usecases/get_weather_data.dart';
-// import './weather_data_event.dart';
-// import './weather_data_state.dart';
-
-// class WeatherDataBloc extends Bloc<WeatherDataEvent, WeatherDataState> {
-//   final GetWeatherData getWeatherData;
-
-//   WeatherDataBloc({required this.getWeatherData}) : super(WeatherDataInitial());
-
-//   // @override
-//   // WeatherDataState get initialState => WeatherDataInitial();
-
-//   // @override
-//   Stream<WeatherDataState> mapEventToState(WeatherDataEvent event) async* {
-//     if (event is GetWeatherDataEvent) {
-//       print('loading');
-//       yield WeatherDataLoading();
-//       final response = await getWeatherData(const Params(location: 'Berlin'));
-//       print('loaded');
-//       yield response.fold(
-//           (failure) => const WeatherDataError('Failed to fetch weather data'),
-//           (weather) => WeatherDataLoaded(weather));
-//     }
-//   }
-// }
-
 import 'package:bloc/bloc.dart';
 // import 'package:equatable/equatable.dart';
 import '../../domain/usecases/get_weather_data.dart';
@@ -46,9 +17,10 @@ class WeatherDataBloc extends Bloc<WeatherDataEvent, WeatherDataState> {
     try {
       final weather = await getWeatherData(Params(location: event.location));
       weather.fold(
-          (failure) => const WeatherDataError('Failed to fetch weather data'),
-          (weather) => WeatherDataLoaded(weather));
-      //  emit(WeatherDataLoaded(weather));
+        (failure) =>
+            emit(const WeatherDataError('Failed to fetch weather data')),
+        (weather) => emit(WeatherDataLoaded(weather)),
+      );
     } catch (error) {
       emit(WeatherDataError(error.toString()));
     }
--- lib/features/weather_data/presentation/pages/weather_data_page.dart
+++ lib/features/weather_data/presentation/pages/weather_data_page.dart
@@ -32,7 +32,52 @@ Widget buildBody(BuildContext context) {
               } else if (state is WeatherDataLoading) {
                 return const CircularProgressIndicator();
               } else if (state is WeatherDataLoaded) {
-                return const Text('Data loaded');
+                // return const Text('Data loaded');
+                final weather = state.weather;
+                return Column(
+                  children: [
+                    Text(
+                      '${weather.location?.name}, ${weather.location?.country}',
+                      style: const TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    // Image.network(weather
+                    //     .current.weatherIcons[0]), // Display weather icon
+                    // const SizedBox(height: 10),
+                    Text(
+                      'Temperature: ${weather.current?.temperature}°C',
+                      style: const TextStyle(
+                          fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '${weather.location?.region}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Humidity: ${weather.current?.humidity}%',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'Wind Speed: ${weather.current?.windSpeed} km/h',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'Wind Direction: ${weather.current?.windDir}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'Pressure: ${weather.current?.pressure} hPa',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'UV Index: ${weather.current?.uvIndex}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ],
+                );
               } else if (state is WeatherDataError) {
                 return Text('Error: ${state.message}');
               } else {
@@ -46,7 +91,7 @@ Widget buildBody(BuildContext context) {
               // Dispatch the event to fetch weather data
               context
                   .read<WeatherDataBloc>()
-                  .add(const GetWeatherDataEvent('Berlin'));
+                  .add(const GetWeatherDataEvent('Moscow'));
             },
             child: const Text('Get Weather'),
           ),
